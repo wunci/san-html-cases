b5cfbb3cbe6935b95fb3fe5ca224f8c338198514
errorrik <errorrik@gmail.com>
2022-05-17 00:03:57 +0800

    fix spec 4 old id

--- src/tplcmpt-style-merge/spec.js
+++ src/tplcmpt-style-merge/spec.js
@@ -2,18 +2,18 @@ it("template component outer style merge inner style", function (done) {
     // [inject] init
 
     var b = wrap.getElementsByTagName('b')[0];
-    expect(b.style.cssText).toContain('1rem;');
-    expect(b.style.cssText).toContain('10px');
-    expect(b.style.cssText).toContain('block');
+
+    expect(/width\s*:\s*30px/i.test(b.style.cssText)).toBeTruthy();
+    expect(/height\s*:\s*10px/i.test(b.style.cssText)).toBeTruthy();
+    expect(/display\s*:\s*block/i.test(b.style.cssText)).toBeTruthy();
 
     myComponent.data.set('sty', "height:20px");
     myComponent.data.set('styi', "width:15px;");
 
     myComponent.nextTick(function () {
-        expect(b.style.cssText).toContain('20px');
-        expect(b.style.cssText).toContain('15px');
-        expect(b.style.cssText).toContain('width');
-        expect(b.style.cssText).not.toContain('block');
+        expect(/width\s*:\s*15px/i.test(b.style.cssText)).toBeTruthy();
+        expect(/height\s*:\s*20px/i.test(b.style.cssText)).toBeTruthy();
+        expect(/display\s*:\s*block/i.test(b.style.cssText)).toBeFalsy();
 
         myComponent.dispose();
         document.body.removeChild(wrap);
