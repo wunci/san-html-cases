d65524af661b78a91bd8ea5cae71de444c85c946
meixg <meixg@foxmail.com>
2023-03-11 20:35:19 +0800

    add cmpt-ssr-render-only-slot

---
+++ src/cmpt-ssr-render-only-slot2/component.js
@@ -0,0 +1,30 @@
+var san = require('san');
+
+var SubChild = san.defineComponent({
+    template: '<i>{{text}}</i>',
+    ssr: 'render-hydrate'
+});
+
+var Child = san.defineComponent({
+    components: {
+        'ui-sub': SubChild
+    },
+    ssr: 'render-hydrate',
+    template: '<p><ui-sub text="{{dt}}"/><slot/></p>'
+});
+
+var ULabel = san.defineComponent({
+    template: '<u>{{text}}</u>',
+    ssr: 'render-hydrate'
+});
+
+var MyComponent = san.defineComponent({
+    components: {
+        'ui-u': ULabel,
+        'ui-c': Child
+    },
+    ssr: 'render-only',
+    template: '<ui-c dt="{{name}}"><a><ui-u text="{{email}}"/></a></ui-c>'
+});
+
+exports = module.exports = MyComponent;
