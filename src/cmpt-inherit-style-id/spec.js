7176215369d4c66f6a5e48cc95eadc77b2305277
errorrik <errorrik@gmail.com>
2024-01-22 10:18:00 +0800

    opti expect

--- src/cmpt-inherit-style-id/spec.js
+++ src/cmpt-inherit-style-id/spec.js
@@ -5,20 +5,21 @@ it("Component Attribute Inherit id、class、style", function (done) {
     expect(spanA.innerHTML).toContain('Hahaha');
     expect(spanA.getAttribute('class')).toBe('inner');
     expect(spanA.getAttribute('id')).toBe('inner');
-    expect(spanA.getAttribute('style').includes('background: green')).toBeTruthy();
-    expect(spanA.getAttribute('style').includes('color: red')).toBeFalsy();
+
+    expect(spanA.getAttribute('style')).toContain('background: green');
+    expect(spanA.getAttribute('style')).not.toContain('color: red');
 
     var spanB = wrap.getElementsByTagName('span')[1];
     expect(spanB.getAttribute('class')).toBe('inner');
     expect(spanB.getAttribute('id') == null).toBeTruthy();
-    expect(spanA.getAttribute('style').includes('background: green')).toBeTruthy();
-    expect(spanA.getAttribute('style').includes('color: red')).toBeFalsy();
+    expect(spanA.getAttribute('style')).toContain('background: green');
+    expect(spanA.getAttribute('style')).not.toContain('color: red');
 
     var spanC = wrap.getElementsByTagName('span')[2];
     expect(spanC.getAttribute('class')).toBe('inner outer');
     expect(spanA.getAttribute('id')).toBe('inner');
-    expect(spanA.getAttribute('style').includes('background: green')).toBeTruthy();
-    expect(spanA.getAttribute('style').includes('color: red')).toBeFalsy();
+    expect(spanA.getAttribute('style')).toContain('background: green');
+    expect(spanA.getAttribute('style')).not.toContain('color: red');
 
     done();
 });
